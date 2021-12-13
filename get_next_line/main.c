2feb2d0b551f7698350b95fb4f70db14e3f1c16d
Dus_JW <jaewoopk000@naver.com>
2021-12-13 17:57:35 +0900

    Day 17 try GNL with open or read method

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int main()
 {
-	int	fd;
+	long	limit;
+	int		fd;
+	int		fd1;
+	int		fd2;
+	char	buf[BUFFER_SIZE];
 
-	fd = open("test.txt", 0);
+	limit = sysconf(_SC_OPEN_MAX);
+	printf("%ld File Descriptors can be used\n", limit);
+
+	fd = open("test.txt",1);
+	printf("%d fd return value\n",fd);
 	close(fd);
+	
+	fd1 = open("test.txt",O_RDONLY);
+	if (fd1 == -1)
+		return (1);
+	printf("fd1\t: %d\n",fd1);
+	printf("\nDuplication\n");
+	fd2 = dup2(fd1, 42);
+	printf("fd1\t: %d\n",fd1);
+	printf("fd2\t: %d\n",fd2);
+	close(fd1);
+	close(fd2);
+
+	fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
+		printf("file open error\n");
+	else
+	{
+		read(fd, buf, sizeof(buf));
+		printf("%s\n",buf);
+		close(fd);
+	}
 	return (0);
 }

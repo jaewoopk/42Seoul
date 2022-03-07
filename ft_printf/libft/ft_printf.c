e6625701099e612c593456eac655779644b00989
Dus_JW <jaewoopk000@naver.com>
2022-03-07 20:28:08 +0900

    Day 34 ft_printf trying to understand varg

--- ft_printf/libft/ft_printf.c
+++ ft_printf/libft/ft_printf.c
@@ -1,12 +1,10 @@
 #include "ft_printf.h"
-#include <string.h>
-#include <stdio.h>
 
 int	ft_printf(const char *first, ...);
 
 int	main(void)
 {
-	ft_printf("abcdefg",'a','b','c','d');
+	ft_printf("alaltodl\n");
 	return (0);
 }
 
@@ -14,15 +12,11 @@ int	ft_printf(const char *first, ...)
 {
 	va_list	ap;
 
-	printf("%p\n",&first);	
-	printf("%p\n",&first + 1);
 	va_start(ap,first);
-	printf("%p\n",&ap);
-	for (int i = 0; i < strlen(first); i++)
+	for (int i = 0; i < ft_strlen(first); i++)
 	{
 		char	c;
 		c = va_arg(ap, int);
-		printf("%c\n",c);
 	}
 	va_end(ap);
 	return (0);

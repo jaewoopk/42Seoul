6c93d3d0ce1fd5959612f796494f144a87827f5a
Dus_JW <jaewoopk000@naver.com>
2022-03-23 14:19:07 +0900

    Day 35 trying to test va 2

--- ft_printf/test_va.c
+++ ft_printf/test_va.c
@@ -24,16 +24,36 @@ void	printf_string(int args, ...)
 	for (int i = 0; i < args; i++)
 	{
 		char *str = va_arg(ap, char *);
-		printf("%s ",str);
+		printf("%s %lu",str,sizeof(char *));
 	}
 	va_end(ap);
 
 	printf ("\n");
+}
+
+void	test_printf()
+{
+	int a = 3,b = 7, c = -10;
+	int *p = &a;
+	char	ch = 't';
+	char	*ps = &ch;
+
+	printf("%c %c %c\n",'a','b','c');
+	printf("%s %s\n","play tennis"," is fun!");
+	printf("%p %p %p %p\n",&a,&p,&c,&ps);
+	printf("%d %d %d\n",a,b,c);
+	printf("%i %i %i\n",a,b,c);
+	c = 590;
+	printf("%u %u %u\n",a,b,c);
+	printf("%x %x %x\n",a,b,c);
+	printf("%X %X %X\n",a,b,c);
+	printf("%% %% %%\n");
 }
 int	main(void)
 {
 	printf("%d\n", sum(10,1,2,3,4,5,6,7,8,9,10));
 	printf_string(2, "C", "Language");
 	printf_string(3, "the", "Little", "prince");
+	test_printf();
 	return (0);
 }

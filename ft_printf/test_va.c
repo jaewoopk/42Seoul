545fea469f0ebfdae86212b8bc7ebaf865a517f1
Dus_JW <jaewoopk000@naver.com>
2022-03-23 14:00:31 +0900

    Day 35 trying to test va

--- ft_printf/test_va.c
+++ ft_printf/test_va.c
@@ -16,8 +16,24 @@ int	sum(int count, ...)
 	return (res);
 }
 
+void	printf_string(int args, ...)
+{
+	va_list ap;
+
+	va_start(ap,args);
+	for (int i = 0; i < args; i++)
+	{
+		char *str = va_arg(ap, char *);
+		printf("%s ",str);
+	}
+	va_end(ap);
+
+	printf ("\n");
+}
 int	main(void)
 {
-	printf ("%d\n", sum(10,1,2,3,4,5,6,7,8,9,10));
+	printf("%d\n", sum(10,1,2,3,4,5,6,7,8,9,10));
+	printf_string(2, "C", "Language");
+	printf_string(3, "the", "Little", "prince");
 	return (0);
 }

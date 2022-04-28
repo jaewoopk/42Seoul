c10912a120b54023020ae5141d766bad6b447130
Dus_JW <jaewoopk000@naver.com>
2022-04-28 22:23:00 +0900

    Day 37 Doing ft_printf2

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,83 +1,66 @@
 #include "ft_printf.h"
 
-int	ft_printf(const char *ch, ...);
-int	devide_by_format(va_list ap, char *format);
+int		ft_printf(const char *ch, ...);
+void	devide_by_format(va_list ap, char *format, int *result);
 
-int	ft_printf(const char *format, ...)
+int		ft_printf(const char *format, ...)
 {
-	va_list	ap;
-	int		result;
+	va_list			ap;
+	int				ans;
+	int				*result;
 
-	result = 0;
+	ans = 0;
+	result = &ans;
 	va_start(ap,format);
-	result = devide_by_format(ap, (char *)format);
+	devide_by_format(ap, (char *)format, result);
 	va_end(ap);
 	return (result);
 }
 
-int	devide_by_format(va_list ap, char *format)
+void	devide_by_format(va_list ap, char *format, int *result)
 {
 	char			*s;
-	int				result;
-	int				k;
 
-	result = 0;
-	k = 0;
 	while (*format)
 	{
 		if (*format != '%')
-		{
-			ft_putchar_fd(*format,1);
-			result++;
-		}
+			*result += ft_putchar_fd(*format,1);
 		else
 		{
-			format++;
-			if (!(*format))
+			if (!*(format + 1))
 				break ;
-			else if (*format == 'c')
-				result += ft_putchar_fd(va_arg(ap, int), 1);
-			else if (*format == 's')
+			else if (*(format + 1) == 'c')
+				*result += ft_putchar_fd(va_arg(ap, int), 1);
+			else if (*(format + 1) == 's')
 			{
 				s = va_arg(ap, char *);
 				if (!s)
 				{
 					write(1,"(null)",6);
-					result += 6;
+					*result += 6;
 				}
 				while (*s)
-					result += ft_putchar_fd(*s++,1);
+					*result += ft_putchar_fd(*s++,1);
 			}
-			else if (*format == 'p')
+			else if (*(format + 1) == 'p')
 			{
 				write (1, "0x", 2);
-				result += 2;
-				result += ft_puthexa_pointer(va_arg(ap, unsigned long));
+				*result += 2;
+				*result += ft_puthexa_pointer(va_arg(ap, unsigned long));
 			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				result -= k;
-				k = ft_putnbr_int(va_arg(ap,int));
-				result += k;
-			}
-			else if (*format == 'u')
-			{
-				result -= k;
-				k = ft_putnbr_uint(va_arg(ap, unsigned int));
-				result += k;
-			}
-			else if (*format == 'x')
-				result += ft_puthexa(va_arg(ap, unsigned int));
-			else if (*format == 'X')
-				result += ft_put_uphexa(va_arg(ap, unsigned int));
-			else if (*format == '%')
-			{
-				write (1, "%", 1);
-				result++;
-			}
+			else if (*(format + 1) == 'd' || *(format + 1) == 'i')
+				*result += ft_putnbr_int(va_arg(ap,int));
+			else if (*(format + 1) == 'u')
+				*result += ft_putnbr_uint(va_arg(ap, unsigned int));
+			else if (*(format + 1) == 'x')
+				*result += ft_puthexa(va_arg(ap, unsigned int));
+			else if (*(format + 1) == 'X')
+				*result += ft_put_uphexa(va_arg(ap, unsigned int));
+			else if (*(format + 1) == '%')
+				*result += ft_putchar_fd("%", 1);
+			format++;
 		}
 		format++;
 	}
-	return (result);
 }
 
